7fff45fc9f3adb94b3df5692fb717647234d45c3
Doru Meltei <dorum@qatar.cmu.edu>
2026-06-21 09:15:19 +0300

    first commit

--- src/app/ui/invoices/status.jsx
+++ src/app/ui/invoices/status.jsx
@@ -1,4 +1,4 @@
-import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
+import { CheckIcon, ClockIcon, DocumentIcon, CalendarDaysIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 
 export default function InvoiceStatus({ status }) {
@@ -9,6 +9,9 @@ export default function InvoiceStatus({ status }) {
         {
           'bg-gray-100 text-gray-500': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
+          'bg-red-500 text-white': status === 'late',
+          'bg-blue-500 text-white': status === 'draft',
+          'bg-orange-500 text-white': status === 'overdue',
         },
       )}
     >
@@ -22,6 +25,24 @@ export default function InvoiceStatus({ status }) {
         <>
           Paid
           <CheckIcon className="ml-1 w-4 text-white" />
+        </>
+      ) : null}
+      {status === 'late' ? (
+        <>
+          Late
+          <ClockIcon className="ml-1 w-4 text-white" />
+        </>
+      ) : null}
+      {status === 'draft' ? (
+        <>
+          Draft
+          <DocumentIcon className="ml-1 w-4 text-white" />
+        </>
+      ) : null}
+      {status === 'overdue' ? (
+        <>
+          Overdue
+          <CalendarDaysIcon className="ml-1 w-4 text-white" />
         </>
       ) : null}
     </span>
